a9e1eb91568f7a0ee3fa49a56f9e96182b8705b9
Casey Elizabeth Gilbert <gilbertcasey2@gmail.com>
2025-08-04 14:46:29 -0500

    remove test error

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,9 +4,7 @@ import * as functions from "firebase-functions";
 import {generateEventPlan} from "frankly-agenda-builder";
 
 export const createEventPlan = functions.https.onCall((data, context) => {
-  // const eventPlan = generateEventPlan(JSON.stringify(data.data));
-  const eventPlan = generateEventPlan(JSON.stringify("very bad data"));
-
+  const eventPlan = generateEventPlan(JSON.stringify(data.data));
   logger.info("Recieved an event plan: " + eventPlan);
   return {eventPlan};
 });

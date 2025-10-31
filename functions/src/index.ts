921ae96788bc00e973ab31622e8846ada83e0b03
Casey Elizabeth Gilbert <gilbertcasey2@gmail.com>
2025-10-31 11:18:20 -0400

    deployment stuff

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
 
 import * as logger from "firebase-functions/logger";
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v2";
 import {generateEventPlan} from "frankly-agenda-builder";
 
-export const createEventPlan = functions.https.onCall(async (data, context) => {
-  const eventPlan = await generateEventPlan(JSON.stringify(data.data));
-  logger.info("Recieved an event plan: " + eventPlan);
-  return {eventPlan};
-});
-
+export const agendaBuildEventPlan = functions.https.onCall(
+  async (data) => {
+    const eventPlan = await generateEventPlan(JSON.stringify(data.data));
+    logger.info("Recieved an event plan: " + eventPlan);
+    return {eventPlan};
+  });
